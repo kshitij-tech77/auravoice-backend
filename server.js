43509da4fba6af3df073b201a3a9f0f2d0e79119
kshitijdhamala100@gmail.com <kshitijdhamala100@gmail.com>
2025-12-05 22:39:10 +0545

    Add AI voice assistant using OpenAI

--- server.js
+++ server.js
@@ -1,42 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const { twiml } = require("twilio");
-// require("dotenv").config();
-
-// const app = express();
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.urlencoded({ extended: false })); // Twilio sends form-encoded
-// app.use(express.json());
-
-// // Health check
-// app.get("/", (req, res) => {
-//   res.send("AURAVOICE backend running successfully 🚀");
-// });
-
-// // Twilio voice webhook
-// app.post("/twilio/voice", (req, res) => {
-//   const response = new twiml.VoiceResponse();
-
-//   // Simple first version – later we'll plug OpenAI here
-//   response.say(
-//     {
-//       voice: "Polly.Joanna",
-//       language: "en-US",
-//     },
-//     "Hi! This is AuraVoice A I. Thanks for calling. How can I help you today?"
-//   );
-
-//   res.type("text/xml");
-//   res.send(response.toString());
-// });
-
-// const PORT = process.env.PORT || 10000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -68,7 +29,7 @@ app.post("/twilio/voice", (req, res) => {
       voice: "Polly.Joanna", // we can change voice later
       language: "en-US",
     },
-    "Hi, this is Kshitij. How was your day? How can I help you, dear?"
+    "Hi, this is Kshitij, CEO of AuraVoice ! How was your day? How can I help you, dear?"
   );
 
   res.type("text/xml");
